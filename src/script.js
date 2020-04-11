cda34a2856f1c004ea3c18d60dc9d046af6e7c30
Wessel Smit <wrsmit00@gmail.com>
2020-04-11 15:09:20 +0200

    added basic video functions

--- src/script.js
+++ src/script.js
@@ -1,15 +1,50 @@
+//Variables
 const video = document.querySelector("video")
 
 
+//Get video info
+document.onkeypress = e => {
+	e = e || window.event
+	const pressedKey = e.code
+	const infoKey = "KeyF"
 
-// * Testing purposes
-// video.volume = 0.1
-video.volume = 0
+	if (pressedKey === infoKey) {
+		console.log("current video time: ", getCurrentTime())
+		console.log("video playing: ", isVideoPlaying())
+		console.log("video aspect ratio: ", getVideoAspectRatio())
+		console.log("---------------------------------")
+	}
+}
 
-video.addEventListener("loadedmetadata", function (e) {
-	const width = this.videoWidth
-	const height = this.videoHeight
 
-	console.log("video aspect ratio: " + (width / 80) + "/" + (height / 80))
-}, false)
-// * Testing purposes
+
+//Helper functions
+function getCurrentTime() {
+	const currentTime = video.currentTime
+	return currentTime
+}
+
+function setVideoTime(time) {
+	video.currentTime = time //time in seconds
+	console.log("set video time: ", time)
+}
+
+function isVideoPlaying() {
+	return video.playing
+}
+
+function getVideoAspectRatio() {
+	const width = video.videoWidth
+	const height = video.videoHeight
+	const aspectRatio = (width / 80) + "/" + (height / 80)
+	return aspectRatio
+}
+
+
+
+//Video properties
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+	get: function () {
+		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2)
+	}
+})
