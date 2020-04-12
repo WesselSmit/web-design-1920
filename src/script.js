175f340dbba3f2ccedeba366f332226bc70aef2a
Wessel Smit <wrsmit00@gmail.com>
2020-04-12 02:13:20 +0200

    added CC json

--- src/script.js
+++ src/script.js
@@ -4,52 +4,37 @@ document.querySelector('video').volume = 0.1
 
 //Variables
 const video = document.querySelector("video")
+let CC
 
 
 
-//Update if video is playing
-video.addEventListener('timeupdate', e => {
-	console.log("update", e.target.currentTime)
-})
+//Make sure CC-data is available before using it
+fetch('./media/CC/feelSomething.json')
+	.then(res => res.json())
+	.then(json => {
+		CC = json.CC
+		addVideoController()
+	})
 
 
+function addVideoController() {
+	//Update if video is playing
+	// video.addEventListener('timeupdate', e => {
+	// 	const videoTime = e.target.currentTime
+	// console.log("update", e.target.currentTime, CC)
 
+	//TODO: momenteel word de update getriggerd elke keer dat de video timer update (meerdere keren per seconde)
+	//TODO: het probleem hiermee is dat elke seconde de browser 4-5x de CC gaat overschrijven en je dus niet kan animeren
+	//TODO: om dit te fixen moet er een soort check ingebouwd worden die kijkt of de tekst al op beeld staat. als dat zo is moet hij niet geupdate worden
 
-
-
-
-
-//Helper functions
-function getCurrentTime() {
-	const currentTime = video.currentTime
-	return currentTime
-}
-
-function setVideoTime(time) {
-	video.currentTime = time //time in seconds
-	console.log("set video time: ", time)
-}
-
-function isVideoPlaying() {
-	return video.playing
-}
-
-function getVideoAspectRatio() {
-	const width = video.videoWidth
-	const height = video.videoHeight
-	const aspectRatio = (width / 80) + "/" + (height / 80)
-	return aspectRatio
+	// const matchingCC = findCC(videoTime)
+	// checkIfNewCC(matchingCC)
+	// })
 }
 
 
 
 
-
-
-
-//Video properties
-Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
-	get: function () {
-		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2)
-	}
-})
+// function findCC(time) {
+// 	return CC.filter(cc => cc.startTime < time && time < cc.endTime)
+// }
