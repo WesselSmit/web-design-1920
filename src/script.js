d6ebe2f0803d16594b4e807907533b903abd8e62
Wessel Smit <wrsmit00@gmail.com>
2020-04-12 22:29:17 +0200

    CC is now draggable

--- src/script.js
+++ src/script.js
@@ -56,58 +56,11 @@ function addVideoController() {
 
 
 
-
+//Make CC draggable
 const captionsContainer = document.getElementById('captions-container')
-captionsContainer.addEventListener('drag', () => {
-	console.log('ja')
-})
-
-// Make the DIV element draggable:
 dragElement(captionsContainer)
 
-function dragElement(elmnt) {
-	var pos1 = 0,
-		pos2 = 0,
-		pos3 = 0,
-		pos4 = 0
-	if (document.getElementById(elmnt.id + "header")) {
-		// if present, the header is where you move the DIV from:
-		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown
-	} else {
-		// otherwise, move the DIV from anywhere inside the DIV:
-		elmnt.onmousedown = dragMouseDown;
-	}
 
-	function dragMouseDown(e) {
-		e = e || window.event
-		e.preventDefault()
-		// get the mouse cursor position at startup:
-		pos3 = e.clientX
-		pos4 = e.clientY
-		document.onmouseup = closeDragElement
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag
-	}
-
-	function elementDrag(e) {
-		e = e || window.event
-		e.preventDefault()
-		// calculate the new cursor position:
-		pos1 = pos3 - e.clientX
-		pos2 = pos4 - e.clientY
-		pos3 = e.clientX
-		pos4 = e.clientY
-		// set the element's new position:
-		elmnt.style.top = (elmnt.offsetTop - pos2) + "px"
-		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
-	}
-
-	function closeDragElement() {
-		// stop moving when mouse button is released:
-		document.onmouseup = null
-		document.onmousemove = null
-	}
-}
 
 
 
@@ -164,4 +117,54 @@ function resetCC() {
 	videoCC.setAttribute("captions-id", "")
 
 	console.log("CC was reset")
+}
+
+
+
+// Make the CC-div element draggable (taken from: https://www.w3schools.com/howto/howto_js_draggable.asp)
+function dragElement(elmnt) {
+	var pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0
+	if (document.getElementById(elmnt.id + "header")) {
+		// if present, the header is where you move the DIV from:
+		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown
+	} else {
+		// otherwise, move the DIV from anywhere inside the DIV:
+		elmnt.onmousedown = dragMouseDown;
+	}
+
+	function dragMouseDown(e) {
+		e = e || window.event
+		e.preventDefault()
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX
+		pos4 = e.clientY
+		document.onmouseup = closeDragElement
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag
+	}
+
+	function elementDrag(e) {
+		captionsContainer.classList.add("fullSize")
+
+		e = e || window.event
+		e.preventDefault()
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX
+		pos2 = pos4 - e.clientY
+		pos3 = e.clientX
+		pos4 = e.clientY
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px"
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px"
+	}
+
+	function closeDragElement() {
+		captionsContainer.classList.remove("fullSize")
+		// stop moving when mouse button is released:
+		document.onmouseup = null
+		document.onmousemove = null
+	}
 }
