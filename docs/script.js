f60bfe62d30caab087712959c384e2d8f7b35837
Wessel Smit <wrsmit00@gmail.com>
2020-04-21 23:55:16 +0200

    removed dev js code

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,3 @@
-//TODO: remove 
-// * development code: (1 line)
-document.querySelector("video").currentTime = 51
-
-
 //Variables
 const video = document.querySelector("video")
 const videoCC = document.getElementById("captions")
